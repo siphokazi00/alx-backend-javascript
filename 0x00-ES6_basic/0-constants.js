318b7c15f8d603ef625097d4f415fca216e1ad09
siphokazi <usphow@gmail.com>
2024-07-25 08:11:34 +0000

    I prefer const when I can. But sometimes let is okay

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -7,11 +7,9 @@ export function getLast() {
 	return ' is okay';
 }
 
-exposrt function taskNext() {
+export function taskNext() {
 	let combination = 'But sometimes let';
 	combination += getLast();
 
 	return combinaion;
 }
-
-module.exports = { taskFirst, getLast, taskNext };
