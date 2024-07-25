018048f19330a897c87f5c69ab26ca0f37151364
siphokazi <usphow@gmail.com>
2024-07-25 08:06:17 +0000

    I prefer const when I can. But sometimes let is okay

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+export function taskFirst() {
+	const task = 'I prefer const when I can.";
+	return task;
+}
+
+export function getLast() {
+	return ' is okay';
+}
+
+exposrt function taskNext() {
+	let combination = 'But sometimes let';
+	combination += getLast();
+
+	return combinaion;
+}
