26f1544058aff120ad1eb95eacf5a71f3b3eb1dd
siphokazi <usphow@gmail.com>
2024-07-29 19:27:09 +0000

    Rest parameter syntax for functions

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,4 +1,3 @@
-import returnHowManyArguments from './4-rest-parameter.js';
-
-console.log(returnHowManyArguments("one"));
-console.log(returnHowManyArguments("one", "two", 3, "4th"));
+export default function returnHowManyArguments(...args) {
+	return args.length;
+}
